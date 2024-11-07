811519834259f27a34c4c098a227137a0f85e99b
sunghwanki <kish1919@gmail.com>
2024-11-07 10:39:07 -0500

    first commit

---
+++ AISnapStudy/AISnapStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISnapStudyApp.swift
+//  AISnapStudy
+//
+//  Created by sunghwan ki on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AISnapStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
